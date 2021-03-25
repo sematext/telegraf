eb318e97e7aaf77ca1fcbaa04e62a4f03c3ef445
Bojan Smid <bsmid@sematext.com>
2021-03-25 16:27:08 +0100

    introduced batch processors, integrated them into sematext output logic

--- plugins/outputs/sematext/processors/processor.go
+++ plugins/outputs/sematext/processors/processor.go
@@ -2,7 +2,14 @@ package processors
 
 import "github.com/influxdata/telegraf"
 
-// Processor is interface that should be implemented by modules which adjust Telegraf metrics to match Sematext format
-type Processor interface {
+// MetricProcessor is interface that should be implemented by modules which adjust Telegraf metrics to match Sematext
+// format
+type MetricProcessor interface {
+	// Process makes adjustments to a single metric instance to be compliant with Sematext backend
 	Process(metric telegraf.Metric) error
+}
+
+// BatchProcessor is used to execute actions on the level of a whole batch of metrics.
+type BatchProcessor interface {
+	Process(metrics []telegraf.Metric) ([]telegraf.Metric, error)
 }

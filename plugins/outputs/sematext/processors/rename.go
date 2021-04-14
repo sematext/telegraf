0bb4b91584edcf51d67d3fe42288a33324c5986b
rabbitstack <rabbitstack7@gmail.com>
2021-04-14 10:07:19 +0200

    Revert "Revert "SC-11274: ajdust field names""
    This reverts commit 9216fdbac807ff58dbfd5b59f50e7734315f9a90.

--- plugins/outputs/sematext/processors/rename.go
+++ plugins/outputs/sematext/processors/rename.go
@@ -6,82 +6,83 @@ import (
 
 var (
 	measurementReplaces = map[string]string{
-		"phpfpm":  "php",
-		"mongodb": "mongo",
-		"apache":  "apache",
-		"nginx":   "nginx",
+		"phpfpm":           "php",
+		"mongodb":          "mongo",
+		"mongodb_db_stats": "mongo",
+		"apache":           "apache",
+		"nginx":            "nginx",
 	}
 	fieldReplaces = map[string]string{
 		// apache
-		"apache.BusyWorkers":          "apache.workers.busy",
-		"apache.BytesPerReq":          "apache.bytes",
-		"apache.ReqPerSec":            "apache.requests",
-		"apache.ConnsAsyncClosing":    "apache.connections.async.closing",
-		"apache.ConnsAsyncKeepAlive":  "apache.connections.async.keepAlive",
-		"apache.ConnsAsyncWriting":    "apache.connections.async.writing",
-		"apache.ConnsTotal":           "apache.connections",
-		"apache.IdleWorkers":          "apache.workers.idle",
-		"apache.scboard_closing":      "apache.workers.closing",
-		"apache.scboard_dnslookup":    "apache.workers.dns",
-		"apache.scboard_finishing":    "apache.workers.finishing",
-		"apache.scboard_idle_cleanup": "apache.workers.cleanup",
-		"apache.scboard_keepalive":    "apache.workers.keepalive",
-		"apache.scboard_logging":      "apache.workers.logging",
-		"apache.scboard_open":         "apache.workers.open",
-		"apache.scboard_reading":      "apache.workers.reading",
-		"apache.scboard_sending":      "apache.workers.sending",
-		"apache.scboard_starting":     "apache.workers.starting",
-		"apache.scboard_waiting":      "apache.workers.waiting",
-		"php.accepted_conn":           "php.fpm.requests.accepted.conns",
-		"php.listen_queue":            "php.fpm.queue.listen",
-		"php.max_listen_queue":        "php.fpm.queue.listen.max",
-		"php.listen_queue_len":        "php.fpm.queue.listen.len",
-		"php.idle_processes":          "php.fpm.process.idle",
-		"php.active_processes":        "php.fpm.process.active",
-		"php.total_processes":         "php.fpm.process.total",
-		"php.max_active_processes":    "php.fpm.process.active.max",
-		"php.max_children_reached":    "php.fpm.process.childrenReached.max",
-		"php.slow_requests":           "php.fpm.requests.slow",
+		"apache.BusyWorkers":          "workers.busy",
+		"apache.BytesPerReq":          "bytes",
+		"apache.ReqPerSec":            "requests",
+		"apache.ConnsAsyncClosing":    "connections.async.closing",
+		"apache.ConnsAsyncKeepAlive":  "connections.async.keepAlive",
+		"apache.ConnsAsyncWriting":    "connections.async.writing",
+		"apache.ConnsTotal":           "connections",
+		"apache.IdleWorkers":          "workers.idle",
+		"apache.scboard_closing":      "workers.closing",
+		"apache.scboard_dnslookup":    "workers.dns",
+		"apache.scboard_finishing":    "workers.finishing",
+		"apache.scboard_idle_cleanup": "workers.cleanup",
+		"apache.scboard_keepalive":    "workers.keepalive",
+		"apache.scboard_logging":      "workers.logging",
+		"apache.scboard_open":         "workers.open",
+		"apache.scboard_reading":      "workers.reading",
+		"apache.scboard_sending":      "workers.sending",
+		"apache.scboard_starting":     "workers.starting",
+		"apache.scboard_waiting":      "workers.waiting",
+		"php.accepted_conn":           "fpm.requests.accepted.conns",
+		"php.listen_queue":            "fpm.queue.listen",
+		"php.max_listen_queue":        "fpm.queue.listen.max",
+		"php.listen_queue_len":        "fpm.queue.listen.len",
+		"php.idle_processes":          "fpm.process.idle",
+		"php.active_processes":        "fpm.process.active",
+		"php.total_processes":         "fpm.process.total",
+		"php.max_active_processes":    "fpm.process.active.max",
+		"php.max_children_reached":    "fpm.process.childrenReached.max",
+		"php.slow_requests":           "fpm.requests.slow",
 		// nginx
-		"nginx.accepts":  "nginx.requests.connections.accepted",
-		"nginx.handled":  "nginx.requests.connections.handled",
-		"nginx.active":   "nginx.requests.connections.active",
-		"nginx.reading":  "nginx.requests.connections.reading",
-		"nginx.writing":  "nginx.requests.connections.writing",
-		"nginx.waiting":  "nginx.requests.connections.waiting",
-		"nginx.requests": "nginx.request.count",
+		"nginx.accepts":  "requests.connections.accepted",
+		"nginx.handled":  "requests.connections.handled",
+		"nginx.active":   "requests.connections.active",
+		"nginx.reading":  "requests.connections.reading",
+		"nginx.writing":  "requests.connections.writing",
+		"nginx.waiting":  "requests.connections.waiting",
+		"nginx.requests": "request.count",
 		// mongodb
-		"mongo.flushes":                   "mongo.flushes",
-		"mongo.flushes_total_time_ns":     "mongo.flushes.time",
-		"mongo.document_inserted":         "mongo.documents.inserted",
-		"mongo.document_updated":          "mongo.documents.updated",
-		"mongo.document_deleted":          "mongo.documents.deleted",
-		"mongo.document_returned":         "mongo.documents.returned",
-		"mongo.resident_megabytes":        "mongo.memory.resident",
-		"mongo.vsize_megabytes":           "mongo.memory.virtual",
-		"mongo.mapped_megabytes":          "mongo.memory.mapped",
-		"mongo.inserts":                   "mongo.ops.insert",
-		"mongo.queries":                   "mongo.ops.query",
-		"mongo.updates":                   "mongo.ops.update",
-		"mongo.getmores":                  "mongo.ops.getmore",
-		"mongo.commands":                  "mongo.ops.command",
-		"mongo.repl_inserts":              "mongo.replica.ops.insert",
-		"mongo.repl_queries":              "mongo.replica.ops.query",
-		"mongo.repl_updates":              "mongo.replica.ops.update",
-		"mongo.repl_deletes":              "mongo.replica.ops.delete",
-		"mongo.repl_getmores":             "mongo.replica.ops.getmore",
-		"mongo.repl_commands":             "mongo.replica.ops.command",
-		"mongo.count_command_failed":      "mongo.commands.failed",
-		"mongo.count_command_total":       "mongo.commands.total",
-		"mongo.data_size":                 "mongo.database.data.size",
-		"mongo.storage_size":              "mongo.database.storage.size",
-		"mongo.index_size":                "mongo.database.index.size",
-		"mongo.collections":               "mongo.database.collections",
-		"mongo.objects":                   "mongo.database.objects",
-		"mongo.connections_current":       "mongo.network.connections",
-		"mongo.connections_total_created": "mongo.network.connections.total",
-		"net_in_bytes":                    "mongo.network.transfer.rx.rate",
-		"net_out_bytes":                   "mongo.network.transfer.tx.rate",
+		"mongo.flushes":                   "flushes",
+		"mongo.flushes_total_time_ns":     "flushes.time",
+		"mongo.document_inserted":         "documents.inserted",
+		"mongo.document_updated":          "documents.updated",
+		"mongo.document_deleted":          "documents.deleted",
+		"mongo.document_returned":         "documents.returned",
+		"mongo.resident_megabytes":        "memory.resident",
+		"mongo.vsize_megabytes":           "memory.virtual",
+		"mongo.mapped_megabytes":          "memory.mapped",
+		"mongo.inserts":                   "ops.insert",
+		"mongo.queries":                   "ops.query",
+		"mongo.updates":                   "ops.update",
+		"mongo.getmores":                  "ops.getmore",
+		"mongo.commands":                  "ops.command",
+		"mongo.repl_inserts":              "replica.ops.insert",
+		"mongo.repl_queries":              "replica.ops.query",
+		"mongo.repl_updates":              "replica.ops.update",
+		"mongo.repl_deletes":              "replica.ops.delete",
+		"mongo.repl_getmores":             "replica.ops.getmore",
+		"mongo.repl_commands":             "replica.ops.command",
+		"mongo.count_command_failed":      "commands.failed",
+		"mongo.count_command_total":       "commands.total",
+		"mongo.data_size":                 "database.data.size",
+		"mongo.storage_size":              "database.storage.size",
+		"mongo.index_size":                "database.index.size",
+		"mongo.collections":               "database.collections",
+		"mongo.objects":                   "database.objects",
+		"mongo.connections_current":       "network.connections",
+		"mongo.connections_total_created": "network.connections.total",
+		"mongo.net_in_bytes":              "network.transfer.rx.rate",
+		"mongo.net_out_bytes":             "network.transfer.tx.rate",
 	}
 )
 
@@ -101,14 +102,25 @@ func (r *Rename) Process(points []telegraf.Metric) ([]telegraf.Metric, error) {
 			continue
 		}
 		point.SetName(replace)
+		removedFields := make([]string, 0)
 		for _, field := range point.FieldList() {
 			key := point.Name() + "." + field.Key
 			replace, ok := fieldReplaces[key]
 			if !ok {
 				continue
 			}
-			point.RemoveField(field.Key)
+			// we can't remove the fields
+			// while iterating because it
+			// produces unwanted effects
+			// e.g. metrics that have the
+			// mapping are not renamed.
+			// That's why we have to remove
+			// them in a separate loop
+			removedFields = append(removedFields, field.Key)
 			point.AddField(replace, field.Value)
+		}
+		for _, f := range removedFields {
+			point.RemoveField(f)
 		}
 	}
 	return points, nil
